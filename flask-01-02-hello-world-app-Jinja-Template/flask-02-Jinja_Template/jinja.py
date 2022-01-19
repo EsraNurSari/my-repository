af866f9e390a6373b355ba7e708eda88748f26c0
EsraNurSari <enursari@gmail.com>
2022-01-18 22:05:11 -0500

    my first flask app

--- flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -4,10 +4,14 @@ from flask import Flask, render_template
 #Create an object named `app` from imported Flask module
 app = Flask(__name__)
 
+#Create a function named `head` which sends number `number1` and `number2` variables to the `index.html`. 
+#Use these variables into the `index.html` file. Assign a URL route the `head` function with decorator `@app.route('/')`
 @app.route("/")
 def head():
     return render_template("index.html", number1=34, number2=45)
 
+#Create a function named `number` which sends number `num1` and `num2` and sum of them to the `index.html`. Use these variables into the `body.html` file. 
+#Assign a URL route the `number` function with decorator `@app.route('/sum')`
 @app.route("/function")
 def function():
     variable1 = 12
